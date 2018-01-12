c3caf17b5db8fa93f4fbd7f47ff046ac096e085e
Ammar Tinwala <ammar@perfect108.com>
2018-01-12 21:31:08 +0530

    Areas generated for trackmaps

--- dashboard/trackmap.js
+++ dashboard/trackmap.js
@@ -1,5 +1,5 @@
 function generateTrackmap(fromDt, toDt) {
-    var furl ="http://18.216.208.225:3001/v1/tracker/motion/installation/5a4e2d0b9963080006dc9dfb/compose/" + fromDt + "/" + toDt + "?st=00:00&et=23:59&blackpoint=25&whitepoint=1000";
+    var furl = "http://18.216.208.225:3001/v1/tracker/motion/installation/5a4e2d0b9963080006dc9dfb/compose/" + fromDt + "/" + toDt + "?st=00:00&et=23:59&blackpoint=25&whitepoint=1000";
 
     var xhr = new XMLHttpRequest();
     xhr.open('GET', furl, true);
@@ -13,7 +13,7 @@ function generateTrackmap(fromDt, toDt) {
     xhr.onload = function (e) {
         if (this.status == 200) {
             var data = btoa(String.fromCharCode.apply(null, new Uint8Array(this.response)));
-            $("#trackmap").html('<img src="data:image/png;base64,' + data + '" />');
+            drawArea(data);
 
             // var canvas = getImageFor32BitInteger(this.response);
             // let urlForImage = canvas.toDataURL();
@@ -41,4 +41,137 @@ function getImageFor32BitInteger(data) {
     // update canvas with new data
     ctx.putImageData(idata, 0, 0);
     return canvas;
+}
+
+function drawArea(data) {
+    var canvas, context, canvaso, contexto;
+
+    // The active tool instance.
+    var tool;
+    var boxCoordintates;
+
+    function init() {
+        // Find the canvas element.
+        canvaso = document.getElementById('trackmap');
+        if (!canvaso) {
+            alert('Error: I cannot find the canvas element!');
+            return;
+        }
+
+        if (!canvaso.getContext) {
+            alert('Error: no canvas.getContext!');
+            return;
+        }
+
+        // Get the 2D canvas context.
+        contexto = canvaso.getContext('2d');
+        if (!contexto) {
+            alert('Error: failed to getContext!');
+            return;
+        }
+
+
+        var image = new Image();
+        image.onload = function () {
+            contexto.drawImage(image, 0, 0);
+        };
+        image.src = "data:image/png;base64," + data;
+
+        // Add the temporary canvas.
+        var container = canvaso.parentNode;
+        canvas = document.createElement('canvas');
+        if (!canvas) {
+            alert('Error: I cannot create a new canvas element!');
+            return;
+        }
+
+        canvas.id = 'imageTemp';
+        canvas.width = canvaso.width;
+        canvas.height = canvaso.height;
+        container.appendChild(canvas);
+
+        context = canvas.getContext('2d');
+
+        // Attach the mousedown, mousemove and mouseup event listeners.
+        canvas.addEventListener('mousedown', ev_canvas, false);
+        canvas.addEventListener('mousemove', ev_canvas, false);
+        canvas.addEventListener('mouseup', ev_canvas, false);
+
+        tool = new tools["rect"]();
+    }
+
+    // The general-purpose event handler. This function just determines the mouse 
+    // position relative to the canvas element.
+    function ev_canvas(ev) {
+        if (ev.layerX || ev.layerX == 0) { // Firefox
+            ev._x = ev.layerX;
+            ev._y = ev.layerY;
+        }
+
+        // Call the event handler of the tool.
+        var func = tool[ev.type];
+        if (func) {
+            func(ev);
+        }
+    }
+
+    // The event handler for any changes made to the tool selector.
+    function ev_tool_change(ev) {
+        if (tools[this.value]) {
+            tool = new tools[this.value]();
+        }
+    }
+
+    // This function draws the #imageTemp canvas on top of #imageView, after which 
+    // #imageTemp is cleared. This function is called each time when the user 
+    // completes a drawing operation.
+    function img_update() {
+        contexto.drawImage(canvas, 0, 0);
+        context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    // This object holds the implementation of each drawing tool.
+    var tools = {};
+
+    // The rectangle tool.
+    tools.rect = function () {
+        var tool = this;
+        this.started = false;
+
+        this.mousedown = function (ev) {
+            tool.started = true;
+            tool.x0 = ev._x;
+            tool.y0 = ev._y;
+        };
+
+        this.mousemove = function (ev) {
+            if (!tool.started) {
+                return;
+            }
+
+            var x = Math.min(ev._x, tool.x0),
+                y = Math.min(ev._y, tool.y0),
+                w = Math.abs(ev._x - tool.x0),
+                h = Math.abs(ev._y - tool.y0);
+
+            context.clearRect(0, 0, canvas.width, canvas.height);
+
+            if (!w || !h) {
+                return;
+            }
+
+            context.strokeRect(x, y, w, h);
+        };
+
+        this.mouseup = function (ev) {
+            if (tool.started) {
+                tool.mousemove(ev);
+                tool.started = false;
+                img_update();
+            }
+        };
+    };
+
+    init();
+
 }
